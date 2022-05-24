2bf3cfa63ef396383039c97fcd77831f6f57889b
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-24 17:02:49 +0430

    first commit

---
+++ Js/script.js
@@ -0,0 +1,26 @@
+/* clock of calculator */
+function currentTime() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let session = "AM";
+  
+    if (hh === 0) {
+      hh = 12;
+    }
+    if (hh > 12) {
+      hh = hh - 12;
+      session = "PM";
+    }
+  
+    hh = hh < 10 ? "0" + hh : hh;
+    mm = mm < 10 ? "0" + mm : mm;
+  
+    let time = hh + ":" + mm + " " + session;
+  
+    document.getElementById("clock").innerText = time;
+    setTimeout(function () {
+      currentTime();
+    }, 1000);
+}
+currentTime();
