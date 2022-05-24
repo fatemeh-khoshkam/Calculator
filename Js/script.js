56334b8c12409ffaa1d22e74ac096bf14260fbdc
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-24 21:31:51 +0430

    update style

--- Js/script.js
+++ Js/script.js
@@ -24,3 +24,55 @@ function currentTime() {
     }, 1000);
 }
 currentTime();
+
+
+/* Theme Buttons */
+const body = document.querySelector("body");
+const btnLight = document.querySelector(".btn-light");
+const btnDark = document.querySelector(".btn-dark");
+
+/* const switchThemeFunc = function () {
+  const turnDarkMode = function () {
+    console.log('1')
+    body.classList.add("dark-mode");
+    btnLight.classList.remove("theme--active");
+    btnDark.classList.add("theme--active");  
+  };
+  const turnLightMode = function () {  
+    body.classList.remove("dark-mode");
+    btnDark.classList.remove("theme--active");
+    btnLight.classList.add("theme--active");
+  };
+
+  if (btnLight.classList.contains("theme--active")) {    
+    turnLightMode();
+  } else {  
+    turnDarkMode();
+  }
+};
+btnLight.addEventListener("click", switchThemeFunc);
+btnDark.addEventListener("click", switchThemeFunc); */
+
+
+// function to set a given theme/color-scheme
+/* function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
+document.querySelector('.calculator__theme').addEventListener("click", toggleTheme); */
