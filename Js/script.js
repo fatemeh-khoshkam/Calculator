7b15505285602966c8d4db0dfdd514a3aa30c7b8
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-31 18:39:33 +0430

    update

--- Js/script.js
+++ Js/script.js
@@ -1,33 +1,5 @@
 'use strict'
 
-/* clock of calculator */
-function currentTime() {
-    let date = new Date();
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    let session = "AM";
-  
-    if (hh === 0) {
-      hh = 12;
-    }
-    if (hh > 12) {
-      hh = hh - 12;
-      session = "PM";
-    }
-  
-    hh = hh < 10 ? "0" + hh : hh;
-    mm = mm < 10 ? "0" + mm : mm;
-  
-    let time = hh + ":" + mm + " " + session;
-  
-    document.getElementById("clock").innerText = time;
-    setTimeout(function () {
-      currentTime();
-    }, 1000);
-}
-currentTime();
-
-
 /* Theme Buttons */
 const body = document.querySelector("body");
 const btnLight = document.querySelector(".btn-light");
@@ -71,38 +43,79 @@ const clearKey = document.querySelector('.empty');
 const operatorKeys = document.querySelectorAll('.operatorKey');
 const deleteKey = document.querySelector('.delete');
 const dotKey = document.querySelector('.dot');
+const inputSmall = document.querySelector('.showNumSmall');
+const inputStr = input.value;
+let inputSmallStr = ''; 
 
 numKeys.forEach(num => {
   num.addEventListener("click",() =>{
     //add trim function to delete space of innerhtml
     input.value += num.innerHTML.trim(); 
+    inputSmall.value += num.innerHTML.trim();
+    inputSmallStr = inputSmall.value; 
   });
-
 });
 
-dotKey.addEventListener("click", () => {
-  const inputStr = input.value;
+dotKey.addEventListener("click", () => { 
   if (!inputStr.includes(".")) {
     input.value += ".";
+    inputSmall.value += ".";
+    inputSmallStr = inputSmall.value;
   }
-});
-
-clearKey.addEventListener("click",() =>{
-  input.value = '';
 });
 
 operatorKeys.forEach(operator =>{
   operator.addEventListener("click",() =>{
-    //add trim function to delete space of innerhtml
-    input.value += operator.innerHTML.trim(); 
+      inputSmall.value += operator.innerHTML.trim();
+      inputSmallStr = inputSmall.value;
   })
-
 });
+function checkLastCharInput(){
+  let lastChar = inputSmallStr.charAt(inputSmallStr.length - 1);
+  if(!isNaN(lastChar)){
+    console.log('number');
+    return 'number';
+  }
+  else if((lastChar == '/') || (lastChar == '+')  || (lastChar == '-')  || (lastChar == '%')){
+    console.log('operator');
+    return 'operator';
+  }
+  else if((lastChar == '.')){
+    console.log('dot');
+    return 'dot';
+  }
+}
+
+result.addEventListener('click',()=>{
+ /*  checkLastCharInput();  */
+})
 
 deleteKey.addEventListener("click",() =>{
-   //delete end num/operator of input value
-   input.value = input.value.slice(0, -1);
+  //delete end num/operator of input value
+  input.value = input.value.slice(0, -1);
+  inputSmall.value = inputSmall.value.slice(0, -1);
+});
+
+clearKey.addEventListener("click",() =>{
+  input.value = '';
+  inputSmall.value = '';
 });
 
 
-
+/* clock of calculator */
+(function () {
+  let time;
+  function timeFormat(time) {
+      return (time < 10) ? `0${time}` : time;
+  }
+  function showTime() {
+      let today = new Date(),
+          hour = timeFormat(today.getHours()),
+          min = timeFormat(today.getMinutes());
+      document.querySelector('#clock').innerHTML = hour + ":" + min;
+      time = setTimeout(function () {
+          showTime()
+      }, 500);
+  }
+  showTime();
+})();
